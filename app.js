7d816f4a686fb793b66c4846967ada165f90c2d5
clemk <clemkfr@gmail.com>
2022-11-07 14:08:20 +0100

    ajout annotation + todo

--- app.js
+++ app.js
@@ -4,29 +4,27 @@ const emojis = ["✔️", "✨", "👀", "😭", "👎"];
 let form = document.querySelector('.quiz-form');
 
 
-
+//inputs selec par blocs --------------------------------
 let inputQest1 = document.querySelectorAll('input[name="q1"]')
 let inputQest2 = document.querySelectorAll('input[name="q2"]')
 let inputQest3 = document.querySelectorAll('input[name="q3"]')
 let inputQest4 = document.querySelectorAll('input[name="q4"]')
 let inputQest5 = document.querySelectorAll('input[name="q5"]')
-
-
-
-
-
-let allInputs = document.querySelectorAll('input')
-let buttonSub = document.querySelector('button')
+//-------------------------------
 
 let questionsBloc = document.querySelectorAll('.question-block')
 
+//bonnes réponses par blocs-------------------------------
 let greatAnswer1 = document.querySelector('input[id="Bonaparte"]')
 let greatAnswer2 = document.querySelector('input[id="date1"]')
 let greatAnswer3 = document.querySelector('input[id="date5"]')
 let greatAnswer4 = document.querySelector('input[id="Ljubljana"]')
 let greatAnswer5 = document.querySelector('input[id="m3"]')
+//---------------------------------------
 
-    let allBlocks = [
+
+//info récupérables pour la fonction de verif---------------------
+let allBlocks = [
                         {answers: inputQest1,
                          styleBlock: questionsBloc[0],
                          greatAnswer: greatAnswer1
@@ -52,34 +50,27 @@ let greatAnswer5 = document.querySelector('input[id="m3"]')
                          greatAnswer: greatAnswer5
 
                         }
-                    ]
+                 ]
+//-------------------------------------------------------------
+
 
 let verify = (block)=>{
-
        if(block.greatAnswer.checked){
          block.styleBlock.style.backgroundColor = 'blue'
        }
        else{
        block.styleBlock.style.backgroundColor = 'white'
-       }
-    //    }
-        // if(!inputs.checked){
-        //     console.log('nop')
-        // }
-        // else{
-        //     console.log('yes')
-        // }
-        console.log(block.greatAnswer)
-    
-       
+       }       
 }
 
-
+// mécanique de l'appli---------------------------------------
 form.addEventListener('submit', function(evt){
     evt.preventDefault()
     allBlocks.forEach(tab => {
         verify(tab)
-    });
-
-   
+    });   
 })
+// --------------------------
+
+//TODO 1-implémenter les réaction(emojis)
+//     2-nombre d'essais limités
